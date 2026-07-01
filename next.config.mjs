c51595c6c55c8b0edc06f5c0d3311119d875de67
ADHiik <167276052+Adhik16@users.noreply.github.com>
2026-07-01 21:15:56 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: "/vercel/path0", // match Vercel’s injected path
+    turbopack: {
+      root: "/vercel/path0",
+    },
+  },
+};
 
 export default nextConfig;
-
