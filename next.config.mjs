db05ab825c9611360425efb464d7976e15c717c9
ADHiik <167276052+Adhik16@users.noreply.github.com>
2026-07-01 13:56:34 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 import { fileURLToPath } from "node:url";
 import { dirname } from "node:path";https://github.com/Adhik16/Port/blob/main/next.config.mjs
-const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const __dirname = dirname(__dirname);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    outputFileTracingRoot: dirname(__filename),
     outputFileTracingRoot: dirname(__dirname),
     turbopack: {
+      root: dirname(__filename),
       root: dirname(__dirname),
     },
   },
