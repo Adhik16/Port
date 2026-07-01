7e06f117c981f36d4264238a0d0eaa760e81b884
ADHiik <167276052+Adhik16@users.noreply.github.com>
2026-07-01 09:31:53 +0545

    Add noValidate attribute to contact form
    Updated form element to include noValidate attribute.

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -87,7 +87,7 @@ export function Contact() {
                     </Button>
                   </motion.div>
                 ) : (
-                  <form className="space-y-5" onSubmit={handleSubmit}>
+                  <form className="space-y-5" noValidate="">
                     <div className="grid sm:grid-cols-2 gap-4">
                       <div className="space-y-1.5">
                         <label
